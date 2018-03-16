3fe12eb56d4947f53c34559d32f3d5727610ec97
tianyuningmou <tianyuningmou2009@126.com>
2018-03-16 17:27:31 +0800

    image tools

--- image_dir/imtool.py
+++ image_dir/imtool.py
@@ -17,4 +17,22 @@ CHANGE:  :
 MODIFIED: : @Time : 2018/3/16 下午4:41
 """
 
+from PIL import Image
+import numpy as np
 
+# 图像缩放函数
+def imresize(im, sz):
+    pil_im = Image.fromarray(np.uint8(im))
+    return np.array(pil_im.resize(sz))
+
+# 对一幅灰度图像进行直方图均衡化
+def histeq(im, nbr_bins=256):
+    # 计算图像的直方图
+    im_hist, bins = np.histogram(im.flatten(), nbr_bins, normed=True)
+    # cumulative distribution functions
+    cdf = im_hist.cumsum()
+    # 归一化
+    cdf = 255 * cdf / cdf[-1]
+    # 利用累积分布函数的线性插值，计算新的像素值
+    im2 = np.interp(im.flatten(), bins[:-1], cdf)
+    return im2.reshape(im.shape), cdf
