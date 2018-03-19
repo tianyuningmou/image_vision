126d659c766567445a143b0643bbf7a6629c5ca4
tianyuningmou <tianyuningmou2009@126.com>
2018-03-19 15:19:58 +0800

    scipy_learn

--- image_dir/scipy_learn.py
+++ image_dir/scipy_learn.py
@@ -17,4 +17,27 @@ CHANGE:  :
 MODIFIED: : @Time : 2018/3/19 下午2:46
 """
 
+from PIL import Image
+from numpy import *
+from scipy.ndimage import filters
 
+
+im = array(Image.open('fbb.jpeg').convert('L'))
+im2 = filters.gaussian_filter(im, 5)
+image_g = Image.fromarray(im2)
+image_g.save('fbb_g.jpeg')
+
+# Sobel导数滤波器
+imx = zeros(im.shape)
+filters.sobel(im, 1, imx)
+image_x = Image.fromarray(uint8(imx))
+image_x.save('fbb_x.jpeg')
+
+imy = zeros(im.shape)
+filters.sobel(im, 0, imy)
+image_y = Image.fromarray(uint8(imy))
+image_y.save('fbb_y.jpeg')
+
+magnitude = sqrt(imx**2 + imy**2)
+image_1 = Image.fromarray(uint8(magnitude))
+image_1.save('fbb_1.jpeg')
