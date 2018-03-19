d6e9c1a15cdfc7ad99880166d99e4c9529e184e1
tianyuningmou <tianyuningmou2009@126.com>
2018-03-19 16:09:06 +0800

    scipy_learn

--- image_dir/scipy_learn.py
+++ image_dir/scipy_learn.py
@@ -19,7 +19,7 @@ MODIFIED: : @Time : 2018/3/19 下午2:46
 
 from PIL import Image
 from numpy import *
-from scipy.ndimage import filters
+from scipy.ndimage import filters, measurements, morphology
 
 
 im = array(Image.open('fbb.jpeg').convert('L'))
@@ -54,3 +54,15 @@ imy = zeros(im.shape)
 filters.gaussian_filter(im, (sigma, sigma), (1, 0), imy)
 image_y = Image.fromarray(uint8(imy))
 image_y.save('fbb_gy.jpeg')
+
+# 载入图像，然后使用阈值化操作，以保证处理的图像为二值图像
+im_2 = array(Image.open('fbb.jpeg').convert('L'))
+im_2 = 1*(im_2<128)
+
+labels, nbr_objects = measurements.label(im_2)
+print('Numbers of objects:', nbr_objects)
+
+# 形态学开操作，更好地分离各个对象
+im_open = morphology.binary_opening(im_2, ones((9, 5)), iterations=2)
+labels_open, nbr_objects_open = measurements.label(im_open)
+print('Numbers of objects:', nbr_objects_open)
